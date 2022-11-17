640ccb1db23d99b0e7dc5272d4010d68e5375344
Swapnil More <swapnilmore@iisc.ac.in>
2022-11-17 10:10:45 +0530

    Added CMP profit code

--- basic_df.py
+++ basic_df.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 # import yfinance as yf
 zero = pd.read_csv('./dataset/ZeroTrade.csv')
+n50 = pd.read_csv('./dataset/Nifty50_data.csv', thousands=',')
+
 data = pd.DataFrame(index=zero.index)
 data['Stock'] = zero.symbol
 data['Price'] = zero.price
@@ -21,13 +23,11 @@ data['apparent_avg'] = data['Invested_amt'] / data['cum_qty']
 data['apparent_amt'] = data['apparent_avg'] * data['cum_qty']
 data = data.fillna(0)
 
-
 stock_slice = []
 for i in data['Stock'].drop_duplicates():
     stock_df = data.loc[data['Stock'] == i]
     ind_x = stock_df[stock_df['cum_qty'].map(lambda x: x == 0)].index
     stock_slice.append(stock_df.tail(1).index[0])
-
 
 stock_slice = pd.Index(stock_slice)
 foli0 = data.iloc[stock_slice]
@@ -36,3 +36,17 @@ foli0.loc['Total'] = foli0.sum(numeric_only=True)
 foli0.loc['Total', ['apparent_avg', 'cum_Avg']] = pd.NA
 foli0.loc['Total', ['%Profit']] = 100 * foli0.at['Total', 'cum_profit'] / foli0.at['Total', 'Invested_amt']
 print(foli0[['Invested_amt', 'cum_profit', 'cum_qty', 'apparent_avg', 'cum_Avg', '%Profit']])
+
+
+s50 = set(n50.Symbol)
+s0 = set(foli0.index)
+
+n50 = n50.set_index('Symbol')
+n50 = n50.sort_index()
+slice_bool_n50 = [x in s50.intersection(s0) for x in n50.index]
+slice_bool_foli0 = [x in s50.intersection(s0) for x in foli0.index]
+foli0['CMP'] = n50['Last Traded Price']
+
+foli0['CMP_profit'] = foli0['CMP']*foli0['cum_qty']-foli0['apparent_amt']
+foli0['CMP_diff'] = foli0['CMP']-foli0['apparent_avg']
+print(foli0[['apparent_avg', 'CMP', 'CMP_diff', 'CMP_profit', 'cum_Avg']])
